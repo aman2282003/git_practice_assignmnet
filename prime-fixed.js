f9788ef9bc59208debd751d6524f2e2bf3d0ea84
Susanta Samanta <susanta721467@gmail.com>
2023-10-08 23:08:29 +0530

    add the prime fixed file

---
+++ prime-fixed.js
@@ -0,0 +1,13 @@
+let num=17
+let count=0
+for (let i=1;i<=num;i++){
+    if (num%i===0){
+        count+=1
+    }
+}
+if(count===2){
+    console.log("Prime number")
+}
+else{
+    console.log("Not a prime number")
+}
